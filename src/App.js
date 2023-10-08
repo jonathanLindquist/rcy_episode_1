83f91ccc5d189a1cfeb2ebfc0f96737a32e96caa
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2023-10-08 08:55:09 -0700

    reset

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
 import "./App.css";
 
-const age = 15;
-age >= 18 ? console.log("text") : console.log("something else");
-
 function App() {
-  const age = 19;
-  const isGreen = true;
-
-  const names = ["John", "Jane", "Joe", "Jenny"];
-
-  return (
-    <div className="App">
-      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
-      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
-
-      {isGreen && <button>This is a button, isGreen is true</button>}
-
-      {names.map((name, key) => {
-        return (
-          <h1 key={key}>
-            {name} {key}
-          </h1>
-        );
-      })}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
