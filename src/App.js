7c6600cd6ccb6f91051b37411d8df3250b5bffab
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2023-10-08 08:47:34 -0700

    looping through arrays, setting key prop

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ function App() {
   const age = 19;
   const isGreen = true;
 
+  const names = ["John", "Jane", "Joe", "Jenny"];
+
   return (
     <div className="App">
       {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
       <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
+
+      {isGreen && <button>This is a button, isGreen is true</button>}
+
+      {names.map((name, key) => {
+        return (
+          <h1 key={key}>
+            {name} {key}
+          </h1>
+        );
+      })}
     </div>
   );
 }
