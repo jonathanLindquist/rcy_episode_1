52e0c86d3fc11ac90be5a6b11953d0449047278b
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2023-10-08 18:02:37 -0700

    input value & changing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [age, setAge] = useState(18);
+  const [inputValue, setInputValue] = useState("");
+
+  const increaseAge = () => {
+    setAge(age + 1);
+    console.log(age);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      {age} <button onClick={increaseAge}> Increase Age </button>
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
+    </div>
+  );
 }
 
 export default App;
