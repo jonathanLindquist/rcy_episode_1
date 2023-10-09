bdd55748949bdf2da598a30ac0b6afccef9985ec
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2023-10-09 15:47:59 -0700

    state handler to toggle visibility

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function App() {
   const [age, setAge] = useState(18);
   const [inputValue, setInputValue] = useState("");
+  const [showText, setShowText] = useState(true);
 
   const increaseAge = () => {
     setAge(age + 1);
@@ -15,11 +16,19 @@ function App() {
     console.log(event.target.value);
   };
 
+  const toggleNameHandler = () => {
+    setShowText(!showText);
+  };
+
   return (
     <div className="App">
       {age} <button onClick={increaseAge}> Increase Age </button>
       <input type="text" onChange={handleInputChange} />
       {inputValue}
+      <div>
+        <button onClick={toggleNameHandler}>Show/Hide</button>
+        {showText === true && <h1>My name is Jonathan</h1>}
+      </div>
     </div>
   );
 }
